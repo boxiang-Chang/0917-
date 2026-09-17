2bda0f06234a07f654991e8c2bc81b71f724c4f0
yayaya <D1515418@o365.fcu.edu.tw>
2026-09-17 12:50:55 +0800

    fy

--- D1515418.py
+++ D1515418.py
@@ -1,7 +1,6 @@
 
-for i in range(1):
-    print("-" * 10)
 width,weight = 5,5
 for _ in range(width):
+     print("*" * weight)
    
    
--- D1546954.py
+++ D1546954.py
@@ -1,12 +1,4 @@
-for i in range(1, 5):
+
+for i in range(4, -1, -1):
     print("*" * i)
-for i in range(1):
-    print("-" * 10)
-width,weight = 5,5
-for _ in range(width):
-    print("*" * weight)
-for i in range(1):
-    print("-" * 10)
-spaces = " " * (4 - i)
-for i in range(4, -1,-1):
-    print("*" * i)
+    
